dfa323c157d98b8e51bcd9febf2b60fd87495cef
murielsouza <muriel-souza@live.com>
2022-05-17 17:25:51 -0300

    identity parcial dapper

--- Endpoints/Funcionarios/FuncionarioGetAll.cs
+++ Endpoints/Funcionarios/FuncionarioGetAll.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace LojaAPI.Endpoints.Funcionarios;
 
@@ -8,16 +8,14 @@ public class FuncionarioGetAll
     public static string Template => "/funcionarios"; 
     public static string [] Methods => new string [] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager) {
-       // var users = userManager.Users.ToList();
-       var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList(); //paginação
-        var funcionarios = new List<FuncionarioResponse>();
-        foreach (var user in users) { //muitos acessos, corrigir com paginação 
-            var claims = userManager.GetClaimsAsync(user).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Nome");
-            var username = claimName != null ? claimName.Value : string.Empty;
-            funcionarios.Add(new FuncionarioResponse(user.Email, username));
-        }
+    public static IResult Action(int? page, int? rows, IConfiguration configuration) {
+        var db = new SqlConnection(configuration["ConnectionStrings:DefaultConnection"]);
+        var query = @"SELECT Email, ClaimValue as Nome FROM AspNetUsers u INNER JOIN AspNetUserClaims 
+                    c on u.id = c.UserId and ClaimType = 'Nome' ORDER BY Nome 
+                    OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+        //OFFSET é para fazer paginação utilizando o Dapper
+        //vantagem Dapper já transforma para um objeto nesse caso a colsulta virará um objeto do tipo FuncionarioResponse
+        var funcionarios = db.Query<FuncionarioResponse>(query, new {page, rows}); //objeto anonimo para passar os parametros para a consulta SQL Query, tem q ter o mesmo nome do @ dentro da consulta
         return Results.Ok(funcionarios);
     }
 }
---
+++ Infra/Database/QueryAllUsersWithClaims.cs
@@ -0,0 +1,14 @@
+﻿namespace LojaAPI.Infra.Database;
+
+public class QueryAllUsersWithClaims
+{
+    private readonly IConfiguration configuration;
+
+    public QueryAllUsersWithClaims(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+    public void Execute(int page, int rows) { 
+        
+    }
+}
